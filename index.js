bd219e143bf3d2f4df0cd28a0310a52dd6ac71f9
Mimi <1119186082@qq.com>
2019-11-29 19:42:51 +0800

    Refactor index.js

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const fs = require('fs');
 const path = require('path');
 const { merge } = require('lodash');
 
-function getFileContent(pluginDir, file) {
-  return fs.readFileSync(pluginDir ? path.join(pluginDir, file) : file, 'utf8');
+module.exports = function(hexo, pluginDir) {
+  this.hexo = hexo;
+  this.pluginDir = pluginDir;
+  this.getFilePath = function(file) {
+    return this.pluginDir ? path.resolve(this.pluginDir, file) : file;
+  }
+  this.getFileContent = function(file) {
+    return fs.readFileSync(this.getFilePath(file), 'utf8');
+  }
+  this.defaultConfigFile = function(key, file) {
+    let defaultConfig = yaml.safeLoad(this.getFileContent(file));
+    this.hexo.config[key] = merge(defaultConfig[key], this.hexo.theme.config[key], this.hexo.config[key]);
+    return this.hexo.config[key];
+  }
 }
-
-function defaultConfigFile(hexo, key, pluginDir, file) {
-  let defaultConfig = yaml.safeLoad(getFileContent(pluginDir, file));
-  hexo.config[key] = merge(defaultConfig[key], hexo.theme.config[key], hexo.config[key]);
-  return hexo.config[key];
-}
-
-module.exports.getFileContent = getFileContent;
-module.exports.defaultConfigFile = defaultConfigFile;
