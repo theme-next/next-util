9ce98df8ecb449295f0aaaddf3de2a634aa1f298
Mimi <1119186082@qq.com>
2020-01-04 11:54:42 +0800

    Code style update

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ module.exports = function(hexo, pluginDir) {
   this.pluginDir = pluginDir;
   this.getFilePath = function(file) {
     return this.pluginDir ? path.resolve(this.pluginDir, file) : file;
-  }
+  };
   this.getFileContent = function(file) {
     return fs.readFileSync(this.getFilePath(file), 'utf8');
-  }
+  };
   this.defaultConfigFile = function(key, file) {
     let defaultConfig = file ? yaml.safeLoad(this.getFileContent(file)) : {};
     this.hexo.config[key] = deepmerge(defaultConfig[key], this.hexo.theme.config[key], this.hexo.config[key]);
     return this.hexo.config[key];
-  }
-}
+  };
+};
